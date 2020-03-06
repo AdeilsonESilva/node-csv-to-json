d775c82002915f2cdbe31248692f4a72b2d1becd
Adeilson Silva <adeilson@gmail.com>
2020-03-06 12:10:20 -0300

    - Receber arquivo por argumento

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const XLSX = require("xlsx");
-const workbook = XLSX.readFile("CSV_FILE.CSV", {
-  codepage: 65001
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+args.forEach(arg => {
+  const workbook = XLSX.readFile(arg, {
+    codepage: 65001
+  });
+  const json = XLSX.utils.sheet_to_json(workbook.Sheets.Sheet1);
+  console.log("file: ", arg);
+  console.log("qty", json.length);
+  console.log("json", json);
+  // fs.writeFile()
+  const csvFile = arg.substr(0, arg.lastIndexOf(".")) + ".JSON";
+  fs.writeFile(csvFile, JSON.stringify(json), () => {});
 });
-
-const json = XLSX.utils.sheet_to_json(workbook.Sheets.Sheet1);
-
-console.log(json.length);
